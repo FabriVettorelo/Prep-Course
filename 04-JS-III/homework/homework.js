a25a4034f556e95eb22b47e0629b916de762f4c9
FabriVettorelo <swfabri@gmail.com>
2022-11-10 00:29:52 -0300

    tarea 3 casi lista

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -1,20 +1,25 @@
 // No cambies los nombres de las funciones.
 
+const { addLeadingDotSlash } = require("@11ty/eleventy/src/TemplatePath");
+
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
   // Tu código:
+  return array[0];
 }
 
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  return array [array.length - 1];
 }
 
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+  return array.length;
 }
 
 
@@ -23,6 +28,10 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+for ( i = 0 ; i < array.length ; i++) {
+  array[i] = array[i] + 1;
+}
+return array;
 }
 
 
@@ -30,6 +39,9 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+  array.push(elemento) 
+    return array;
+  
 }
 
 
@@ -38,6 +50,8 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+  array.unshift(elemento)
+  return array
 }
 
 
@@ -47,6 +61,7 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
+  return palabras.join (" ")
 }
 
 
@@ -54,6 +69,13 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+  for ( i = 0 ; i < array.length ; i++) {
+    if (array[i] === elemento) {
+      return true;
+    }  
+  }
+  return false;
+
 }
 
 
@@ -61,6 +83,10 @@ function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+  var suma = 0;
+  for (i=0 ; i < numeros.length ; i++){
+    suma = suma + numeros[i];
+  } return suma;
 }
 
 
@@ -68,13 +94,26 @@ function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
+  var suma = 0; 
+  var promedio = 0;
+  for (var i= 0; i < resultadosTest.length; i++) {
+    suma = (suma + resultadosTest[i]);
+    promedio = suma / resultadosTest.length;
+  }
+  return promedio;
 }
+  
 
 
 function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+  var grande = 0
+  for (var i = 0 ; i<numeros.length ; i++){
+    if (numeros[i] > grande)
+    grande = numeros[i]
+  } return grande
 }
 
 
@@ -82,13 +121,22 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  if (arguments.length < 1) return 0;
+  var total = 1
+  for (var i=0 ; i<arguments.length; i++){
+    total = total*arguments[i];
+  } return total;
 }
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+  var mayora = 0
+  for (var i=0; i<arreglo.length; i++){
+  if (arreglo[i] > 18)
+  mayora = mayora + 1
+} return mayora
 }
 
 
@@ -97,7 +145,12 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  
+  if (numeroDeDia === 1 || numeroDeDia === 7){
+    return "Es fin de semana"
+  }
+  else if (numeroDeDia === 2 ||numeroDeDia === 3 ||numeroDeDia === 4 ||numeroDeDia === 5 ||numeroDeDia === 6) {
+    return "Es dia Laboral"
+  }
 } 
 
 
